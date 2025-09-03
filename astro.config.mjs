ceeb491a825bfcd7c7e034f2986fba0ebc024a04
sameerptb2 <sameerhusainp@gmail.com>
2025-09-04 02:47:07 +0300

    added RSS feed for blog posts and completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astro-tutorial-samy.netlify.app/"
+  site: "https://astro-tutorial-samy.netlify.app/",
+  integrations: [preact()]
 });
